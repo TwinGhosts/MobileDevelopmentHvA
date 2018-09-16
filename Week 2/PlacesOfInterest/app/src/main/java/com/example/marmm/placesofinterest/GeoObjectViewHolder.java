a6e8507b2e0138f2aa4b4419d57c8c1435ccbb87
Joeri Vervaat <joerivervaat94@gmail.com>
2018-09-16 19:35:57 +0200

    Added Marco's (Teacher) Version of PlacesOfInterest
    To compare with my own as my images are really small

---
+++ Week 2/PlacesOfInterest/app/src/main/java/com/example/marmm/placesofinterest/GeoObjectViewHolder.java
@@ -0,0 +1,28 @@
+package com.example.marmm.placesofinterest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by marmm on 02/11/2017.
+ */
+
+public class GeoObjectViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        public TextView geoName;
+        public ImageView geoImage;
+        public View view;
+
+        public GeoObjectViewHolder(View itemView) {
+            super(itemView);
+            geoName = itemView.findViewById(R.id.geoTextView);
+            geoImage =  itemView.findViewById(R.id.geoImageView);
+            view = itemView;
+        }
+    }
+
+
---
+++ Week 2/PlacesOfInterest/app/src/main/java/com/example/marmm/placesofinterest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.marmm.placesofinterest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<GeoObject> mGeoObjects = new ArrayList<>();
+
+        for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
+
+            mGeoObjects.add(new GeoObject(GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i]));
+        }
+
+        RecyclerView mGeoRecyclerView =  findViewById(R.id.recyclerView);
+
+      //  RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2); //2 cells per row
+
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+
+        mGeoRecyclerView.setLayoutManager(mLayoutManager);
+        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        mGeoRecyclerView.setAdapter(mAdapter);
+    }
+
+
+
+}
